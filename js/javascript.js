ac48cd6425ac2e153442bb7b139450d2e5da2560
Yuriy <35668947+YuriyBortnik@users.noreply.github.com>
2020-02-03 16:29:30 +0100

    block two

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-//topMenuOne
+//topMenu
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -8,15 +8,28 @@ function myFunction() {
     }
 }
 
-// topMenu
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
+//button
+var buttons = document.getElementsByClassName('button'),
+    forEach = Array.prototype.forEach;
+
+forEach.call(buttons, function (b) {
+    b.addEventListener('click', addElement);
+});
+
+function addElement(e) {
+    var addDiv  = document.createElement('div'),
+        mValue  = Math.max(this.clientWidth, this.clientHeight),
+        rect    = this.getBoundingClientRect();
+    sDiv    = addDiv.style,
+        px      = 'px';
+
+    sDiv.width  = sDiv.height = mValue + px;
+    sDiv.left  = e.clientX - rect.left - (mValue / 2) + px;
+    sDiv.top   = e.clientY - rect.top - (mValue / 2) + px;
+
+    addDiv.classList.add('pulse');
+    this.appendChild(addDiv);
+}
 
 //Slider
 var btn_prev = document.querySelector('#gallery .buttons .prev');
